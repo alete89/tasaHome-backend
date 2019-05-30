efeba6b2ad20d04fd44ee0d590e6f8adba46d77b
alete89 <maildealete@gmail.com>
2019-05-29 21:19:55 -0300

    bootstrap prueba

--- app/Bootstrap.ts
+++ app/Bootstrap.ts
@@ -1,5 +1,6 @@
 import { Usuario } from "../model/Usuario";
 
 export const usuarios: Usuario[] = [
-    new Usuario({ nombre: "Juan", apellido: "Perez", email: "juanp@mail.com" })
+    new Usuario({ nombre: "Juan", apellido: "Perez", email: "juanp@mail.com" }),
+    new Usuario({ nombre: "Pedro", apellido: "Gutierrez", email: "pedrog@mail.com" })
 ]
