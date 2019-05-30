c711232991784943e8131d5642f08570af96b933
alete89 <maildealete@gmail.com>
2019-05-29 22:51:30 -0300

    endpoints de prueba express+typeorm
    Co-authored-by: Nicolas Viotti <nicolasviotti90@gmail.com>

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,16 @@
 import express = require('express');
 import { usuarios } from './Bootstrap';
 import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { Usuario } from '../model/Usuario';
 
 
+async function crearConexion() {
+    // createConnection method will automatically read connection options
+    // from your ormconfig file or environment variables
+    const connection = await createConnection();
+    return connection
+}
 
 // Create a new express application instance
 const app: express.Application = express();
@@ -11,10 +19,35 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/usuarios/', function (req, res) {
-    res.send(usuarios);
+app.get('/usuarios/', async function (req, res) {
+    res.send(await traerLosUsuarios());
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+async function guardarUnUsuario() {
+    let conexion = await crearConexion()
+    await conexion.manager.save(usuarios)
+}
+
+async function traerLosUsuarios() {
+    try {
+
+        let conexion = await crearConexion()
+        // let usuarios = await conexion.manager
+        let usuarios = await conexion.manager.find(Usuario)
+        // .getRepository(Usuario)
+        // .createQueryBuilder("user")
+        // // .where("user.id = :id", { id: 1 })
+        conexion.close()
+        return usuarios
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+
+// guardarUnUsuario()
