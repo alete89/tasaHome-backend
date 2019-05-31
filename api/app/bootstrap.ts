9f6a66229b28d3decc70876235503f1e0bd84804
nicovio <nicolasviotti90@gmail.com>
2019-05-31 20:25:47 -0300

    feat: amplio un poco bootstrap

--- api/app/bootstrap.ts
+++ api/app/bootstrap.ts
@@ -2,6 +2,9 @@ import { createConnection, getCustomRepository, getRepository, Repository } from
 import { Domicilio } from "../models/domicilio";
 import { Usuario } from "../models/usuario";
 import { RepoUsuarios } from "../repos/repoUsuarios";
+import { Partido } from "../models/partido";
+import { Localidad } from "../models/localidad";
+import { Provincia } from "../models/provincia";
 
 export class Bootstrap {
 
@@ -11,14 +14,17 @@ export class Bootstrap {
     domicilioCeleste: Domicilio
     repoUsuarios: RepoUsuarios
     repoDomicilio: Repository<Domicilio>
+    sanMartin: Partido
+    ballester: Localidad
+    buenosAires: Provincia
 
     async run() {
         try {
             let conexion = await createConnection()
-            this.repoUsuarios = await getCustomRepository(RepoUsuarios);
+            this.repoUsuarios = getCustomRepository(RepoUsuarios);
             let noHayUsuarios = await this.repoUsuarios.noHayUsuarios()
             if (noHayUsuarios) {
-                this.repoDomicilio = await getRepository(Domicilio)
+                this.repoDomicilio = getRepository(Domicilio)
                 await this.crearDomicilios()
                 await this.crearUsuarios()
             }
@@ -30,8 +36,14 @@ export class Bootstrap {
     }
 
     async crearDomicilios() {
-        this.domicilioJuan = new Domicilio({ descripcion: "Posadas 1515" })
-        this.domicilioCeleste = new Domicilio({ descripcion: "Ayacucho 3520" })
+        this.buenosAires = new Provincia({ nombre: "Buenos Aires" })
+        this.sanMartin = new Partido({ nombre: "San Martín" })
+        this.ballester = new Localidad({ nombre: "Villa Ballester" })
+        await getRepository(Provincia).save(this.buenosAires)
+        await getRepository(Partido).save(this.sanMartin)
+        await getRepository(Localidad).save(this.ballester)
+        this.domicilioJuan = new Domicilio({ descripcion: "Posadas 1515", provincia: Promise.resolve(this.buenosAires), partido: Promise.resolve(this.sanMartin), localidad: Promise.resolve(this.ballester) })
+        this.domicilioCeleste = new Domicilio({ descripcion: "Ayacucho 3520", provincia: Promise.resolve(this.buenosAires), partido: Promise.resolve(this.sanMartin), localidad: Promise.resolve(this.ballester) })
         this.domicilios = [this.domicilioJuan, this.domicilioCeleste]
         await this.repoDomicilio.save(this.domicilios)
     }
@@ -39,11 +51,11 @@ export class Bootstrap {
     async crearUsuarios() {
         this.usuarios = [new Usuario({
             nombre: "Juan", apellido: "Perez", edad: 20, email: "juanp@mail.com", genero: "Hombre", contraseña: "123",
-            domicilio: this.domicilioJuan
+            domicilio: Promise.resolve(this.domicilioJuan)
         }),
         new Usuario({
             nombre: "Celeste", apellido: "Cid", edad: 45, email: "celes@mail.com", genero: "Mujer", contraseña: "cel",
-            domicilio: this.domicilioCeleste
+            domicilio: Promise.resolve(this.domicilioCeleste)
         })
         ]
         await this.repoUsuarios.guardarUsuarios(this.usuarios)
