771460b1a38ad06b00d21ba02232f74e18843992
nicovio <nicolasviotti90@gmail.com>
2019-05-30 22:42:07 -0300

    refactor: cambio la estructura del proyecto

--- app/Bootstrap.ts
+++
@@ -1,6 +0,0 @@
-import { Usuario } from "../model/Usuario";
-
-export const usuarios: Usuario[] = [
-    new Usuario({ nombre: "Juan", apellido: "Perez", email: "juanp@mail.com" }),
-    new Usuario({ nombre: "Pedro", apellido: "Gutierrez", email: "pedrog@mail.com" })
-]
