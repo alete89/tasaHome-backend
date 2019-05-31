1b4ff673d8685b173de6b66cbba21174bfb8424c
nicovio <nicolasviotti90@gmail.com>
2019-05-31 02:55:02 -0300

    refactor: paso lógica de controller a repoUsuarios

--- api/repos/repoUsuarios.ts
+++ api/repos/repoUsuarios.ts
@@ -1,3 +1,31 @@
-export class RepoUsuarios {
+import { EntityRepository, Repository } from "typeorm";
+import { Usuario } from "../models/usuario";
 
-} 
+@EntityRepository(Usuario)
+export class RepoUsuarios extends Repository<Usuario> {
+
+    async todosLosUsuarios() {
+        try {
+            return await this.find()
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async noHayUsuarios() {
+        try {
+            return await this.count() == 0
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async guardarUsuarios(usuarios: Usuario[]) {
+        try {
+            await this.save(usuarios)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+}
