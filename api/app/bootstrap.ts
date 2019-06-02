b43656dc7c3dd198bbb2a9b4d30c900aa2300ca3
nicovio <nicolasviotti90@gmail.com>
2019-06-02 03:47:17 -0300

    feat: amplio bootstrap

--- api/app/bootstrap.ts
+++ api/app/bootstrap.ts
@@ -1,14 +1,26 @@
 import { createConnection, getCustomRepository, getRepository, Repository } from "typeorm";
+import { Barrio } from "../models/barrio";
 import { Domicilio } from "../models/domicilio";
+import { Escuela } from "../models/escuela";
+import { Localidad } from "../models/localidad";
+import { Partido } from "../models/partido";
+import { Provincia } from "../models/provincia";
+import { Tasacion } from "../models/tasacion";
 import { Usuario } from "../models/usuario";
 import { RepoUsuarios } from "../repos/repoUsuarios";
-import { Partido } from "../models/partido";
-import { Localidad } from "../models/localidad";
-import { Provincia } from "../models/provincia";
+import { Comisaria } from "../models/comisaria";
+import { EspacioVerde } from "../models/espacio_verde";
+import { Hospital } from "../models/hospital";
+import { TipoPropiedad } from "../models/tipo_propiedad";
+import { TipoOperacion } from "../models/tipo_operacion";
+import { Estado } from "../models/estado";
+import { Servicio } from "../models/servicio";
 
 export class Bootstrap {
 
     usuarios: Usuario[] = []
+    juan: Usuario
+    celeste: Usuario
     domicilios: Domicilio[]
     domicilioJuan: Domicilio
     domicilioCeleste: Domicilio
@@ -17,6 +29,11 @@ export class Bootstrap {
     sanMartin: Partido
     ballester: Localidad
     buenosAires: Provincia
+    villaUrquiza: Barrio
+    tasacion: Tasacion
+    casa: TipoPropiedad
+    venta: TipoOperacion
+    electricidad: Servicio
 
     async run() {
         try {
@@ -25,14 +42,60 @@ export class Bootstrap {
             let noHayUsuarios = await this.repoUsuarios.noHayUsuarios()
             if (noHayUsuarios) {
                 this.repoDomicilio = getRepository(Domicilio)
+                await this.crearBarrios()
+                await this.crearEscuelas()
+                await this.crearComisarias()
+                await this.crearEspacios()
+                await this.crearHospitales()
                 await this.crearDomicilios()
                 await this.crearUsuarios()
+                await this.crearTiposDePropiedad()
+                await this.crearTiposDeOperacion()
+                await this.crearServicios()
+                await this.crearTasaciones()
             }
             conexion.close()
         }
         catch (e) {
             console.log(e)
         }
+    }
+
+    async crearBarrios() {
+        this.villaUrquiza = new Barrio({ descripcion: "Villa Urquiza" })
+        await getRepository(Barrio).save(this.villaUrquiza)
+    }
+
+    async crearEscuelas() {
+        let escuela: Escuela = new Escuela()
+        escuela.barrio = Promise.resolve(this.villaUrquiza)
+        await getRepository(Escuela).save(escuela).catch(function (error) {
+            console.log(error)
+        })
+    }
+
+    async crearComisarias() {
+        let comisaria: Comisaria = new Comisaria()
+        comisaria.barrio = Promise.resolve(this.villaUrquiza)
+        await getRepository(Comisaria).save(comisaria).catch(function (error) {
+            console.log(error)
+        })
+    }
+
+    async crearEspacios() {
+        let espacio_verde: EspacioVerde = new EspacioVerde()
+        espacio_verde.barrio = Promise.resolve(this.villaUrquiza)
+        await getRepository(EspacioVerde).save(espacio_verde).catch(function (error) {
+            console.log(error)
+        })
+    }
+
+    async crearHospitales() {
+        let hospital: Hospital = new Hospital()
+        hospital.barrio = Promise.resolve(this.villaUrquiza)
+        await getRepository(Hospital).save(hospital).catch(function (error) {
+            console.log(error)
+        })
     }
 
     async crearDomicilios() {
@@ -42,25 +105,69 @@ export class Bootstrap {
         await getRepository(Provincia).save(this.buenosAires)
         await getRepository(Partido).save(this.sanMartin)
         await getRepository(Localidad).save(this.ballester)
-        this.domicilioJuan = new Domicilio({ descripcion: "Posadas 1515", provincia: Promise.resolve(this.buenosAires), partido: Promise.resolve(this.sanMartin), localidad: Promise.resolve(this.ballester) })
-        this.domicilioCeleste = new Domicilio({ descripcion: "Ayacucho 3520", provincia: Promise.resolve(this.buenosAires), partido: Promise.resolve(this.sanMartin), localidad: Promise.resolve(this.ballester) })
+        this.domicilioJuan = new Domicilio({
+            descripcion: "Posadas 1515",
+            provincia: Promise.resolve(this.buenosAires), partido: Promise.resolve(this.sanMartin),
+            localidad: Promise.resolve(this.ballester)
+        })
+        this.domicilioCeleste = new Domicilio({
+            descripcion: "Ayacucho 3520",
+            provincia: Promise.resolve(this.buenosAires),
+            partido: Promise.resolve(this.sanMartin),
+            localidad: Promise.resolve(this.ballester)
+        })
         this.domicilios = [this.domicilioJuan, this.domicilioCeleste]
         await this.repoDomicilio.save(this.domicilios)
     }
 
+    async crearTiposDePropiedad() {
+        this.casa = new TipoPropiedad({ descripcion: "Casa" })
+        await getRepository(TipoPropiedad).save(this.casa)
+    }
+
+    async crearTiposDeOperacion() {
+        this.venta = new TipoOperacion({ descripcion: "Venta" })
+        await getRepository(TipoOperacion).save(this.venta)
+    }
+
+    async crearServicios() {
+        this.electricidad = new Servicio({ descripcion: "Electricidad" })
+        await getRepository(Servicio).save(this.electricidad)
+    }
+
+    async crearTasaciones() {
+
+        let estado_propiedad = new Estado({ descripcion: "Bueno" })
+        await getRepository(Estado).save(estado_propiedad)
+
+        this.tasacion = new Tasacion({
+            descripcion: "Tasación prueba",
+            ambientes: 5,
+            superficie: 300,
+            fecha: new Date,
+            barrio: Promise.resolve(this.villaUrquiza),
+            usuario: Promise.resolve(this.celeste),
+            tipoDePropiedad: Promise.resolve(this.casa),
+            tipoDeOperacion: Promise.resolve(this.venta),
+            estado: Promise.resolve(estado_propiedad),
+            servicios: Promise.resolve([this.electricidad])
+        })
+        this.tasacion.calcularValor()
+        await getRepository(Tasacion).save(this.tasacion)
+    }
+
     async crearUsuarios() {
-        this.usuarios = [new Usuario({
+        this.juan = new Usuario({
             nombre: "Juan", apellido: "Perez", edad: 20, email: "juanp@mail.com", genero: "Hombre", contraseña: "123",
             domicilio: Promise.resolve(this.domicilioJuan)
-        }),
-        new Usuario({
+        })
+        this.celeste = new Usuario({
             nombre: "Celeste", apellido: "Cid", edad: 45, email: "celes@mail.com", genero: "Mujer", contraseña: "cel",
             domicilio: Promise.resolve(this.domicilioCeleste)
         })
-        ]
+        this.usuarios = [this.juan, this.celeste]
         await this.repoUsuarios.guardarUsuarios(this.usuarios)
     }
-
 }
 
 
