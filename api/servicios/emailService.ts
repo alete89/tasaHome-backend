86cf9771fe1877c09fa15ff1b4c6fc8d7c751407
nicovio <nicolasviotti90@gmail.com>
2019-06-23 18:30:13 -0300

    feat: email service

---
+++ api/servicios/emailService.ts
@@ -0,0 +1,46 @@
+import { Usuario } from "../models/usuario";
+
+export class EmailService {
+
+    nodeMailer = require('nodemailer')
+
+    emailTasaHome: String = 'tasacioneshome@gmail.com'
+
+    transporter: any
+
+    smtpConfig: any
+
+    constructor() {
+        this.smtpConfig = {
+            service: 'Gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // use SSL
+            auth: {
+                user: 'tasacioneshome@gmail.com',
+                pass: 'tasahome'
+            }
+        }
+        this.transporter = this.nodeMailer.createTransport(this.smtpConfig)
+    }
+
+
+    enviarEmail(emisor: Usuario, email_receptor: String, mensaje: String) {
+
+        let mailOptions = {
+            
+            from: '"TasaHome" <tasacioneshome@gmail.com>',
+            to: email_receptor,
+            subject: 'Nuevo mensaje de ' + emisor.nombre + " " + emisor.apellido,
+            text: mensaje
+        }
+
+        this.transporter.sendMail(mailOptions, function (error: any, info: any) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email enviado: ' + info.response);
+            }
+        })
+    }
+}
