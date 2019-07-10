ac779b61bf5d36f38229452d7e32d2724ec88f6f
Francisco Repetto <frepetto@siu.edu.ar>
2019-07-10 14:20:03 -0300

    Agrega descripcion a escuelas

--- api/models/escuela.ts
+++ api/models/escuela.ts
@@ -17,6 +17,9 @@ export class Escuela {
     @Column("decimal", { nullable: true, precision: 20, scale: 15 })
     latitud: number
 
+    @Column()
+    descripcion: String
+
     @Column({ nullable: true })
     fecha_actualizacion: Date
 
